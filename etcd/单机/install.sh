b6a497e9fd931d7761544115cf471770ea6bcb44
chenyouwei3 <2148275751@qq.com>
2024-07-02 14:42:34 +0800

    etcd初始化

--- etcd/单机/install.sh
+++ etcd/单机/install.sh
@@ -18,9 +18,11 @@ echo "initial-cluster-token: etcd-cluster-token" >> $ETCD_YML_PATH
 echo "initial-cluster-state: new" >> $ETCD_YML_PATH
 echo "log-level: info" >> $ETCD_YML_PATH
 echo "logger: zap" >> $ETCD_YML_PATH
-echo "log-outputs: stderr" >> $ETCD_YML_PATH
+echo 'log-outputs: ["stderr"]' >> $ETCD_YML_PATH
 
 # 提示操作完成
 echo "etcd.yml 文件已更新和保存"
 
-docker pull gcr.io/etcd-development/etcd:v3.4.25
+docker pull quay.io/coreos/etcd:v3.5.12
+
+docker compose up -d
